cc5f3214faf9c1cc058bb6f20d613084a75186e1
DonetsYuliya <donecjuli@gmail.com>
2022-12-13 16:17:26 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { getImages } from './getImages';
 import { renderCardItems } from './markup';
 import axios from 'axios';
-import { axios } from './getImages';
+export const axios = require('axios');
 
 export const API_KEY = '31904814-f4bcbbfe75d97904192d1a917';
 export let page = 0;
